db9331fba810ff808aa1f1ea93e6bbbabb00a310
Binh Ho <binhhtpd08087@fpt.edu.vn>
2023-03-09 16:00:07 +0700

    upload

--- lab1/lab1bai1.js
+++ lab1/lab1bai1.js
@@ -8,13 +8,3 @@ var c = a + b;
 // In ra kết quả đồng thời bằng 2 cách
 document.write("Kết quả của " + a + " + " + b + " = " + c + "<br>");
 console.log("Kết quả của " + a + " + " + b + " = " + c);
-
-// Nhập vào 1 số nguyên d từ bàn phím
-var d = parseInt(prompt("Nhập số nguyên d:"));
-
-// Tính tổng result của c và d
-var result = c + d;
-
-// In ra kết quả đồng thời bằng 2 cách
-alert("Kết quả của " + c + " + " + d + " = " + result);
-console.log("Kết quả của " + c + " + " + d + " = " + result);
