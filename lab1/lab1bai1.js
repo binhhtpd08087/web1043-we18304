cb6db06d9050f5771f4263cbc9da256f0d271f12
ThanhBinhHo <binhhtpd08087@fpt.edu.vn>
2023-03-26 08:04:30 +0700

    init

---
+++ lab1/lab1bai1.js
@@ -0,0 +1,10 @@
+// Nhập vào 2 số nguyên a, b từ bàn phím
+var a = parseInt(prompt("Nhập số nguyên a:"));
+var b = parseInt(prompt("Nhập số nguyên b:"));
+
+// Tính tổng c của a và b
+var c = a + b;
+
+// In ra kết quả đồng thời bằng 2 cách
+document.write("Kết quả của " + a + " + " + b + " = " + c + "<br>");
+console.log("Kết quả của " + a + " + " + b + " = " + c);
